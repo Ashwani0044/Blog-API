d90b20c68c2a8ec024b9181e666bf769c1c3a89f
Ashwani0044 <ashwanib707@gmail.com>
2026-04-18 00:26:37 +0530

    edge cases

--- routes/auth.py
+++ routes/auth.py
@@ -10,9 +10,22 @@ auth_bp = Blueprint('auth', __name__)
 @auth_bp.route('/signup', methods=['POST'])
 def signup():
     data = request.json
+
+    if not data.get('email') or not data.get('password') or not data.get('username'):
+        return jsonify(message='All fields required'), 400
+
+    existing_user = Users.query.filter_by(email=data['email']).first()
+    if existing_user:
+        return jsonify(message='Email already exists'), 400
+
     hashed_password = hashpw(data['password'].encode('utf-8'), gensalt())
 
-    user = Users(username=data['username'], email=data['email'], password=hashed_password.decode('utf-8'))
+    user = Users(
+        username=data['username'],
+        email=data['email'],
+        password=hashed_password.decode('utf-8')
+    )
+
     db.session.add(user)
     db.session.commit()
 
@@ -21,7 +34,7 @@ def signup():
 @auth_bp.route('/login', methods=['POST'])
 def login():
     data = request.json
-    user = Users.query.filter_by(username=data['username']).first()
+    user = Users.query.filter_by(email= data.get('email')).first()
 
     if user and checkpw(data['password'].encode('utf-8'), user.password.encode('utf-8')):
         access_token = create_access_token(identity=str(user.id))
